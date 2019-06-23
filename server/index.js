db48f4ff3e8e7f9e6f2b4d0883f4cd68bb54401a
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-23 18:35:41 +0530

    adding dependencies;

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
  const app = require('express')();
+ const passport = require('passport');
+ const GoogleStrategy = require('passport-google-oauth20');
 
-
- app.get('/', (req,res) => {
-   res.send({ hi: 'there'})
- })
+ 
 
  const PORT = process.env.PORT || 5000;
- app.listen(PORT)
+ app.listen(PORT) 
