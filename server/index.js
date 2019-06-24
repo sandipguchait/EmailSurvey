4fb106eed467b13d2613e4112ce6002201fe3d7c
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-24 21:47:50 +0530

    finishing authentication

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
  const app = require('express')(); //require express
  const mongoose = require('mongoose');
+ const cookieSession = require('cookie-session');
+ const passport = require('passport');
  const keys = require('./config/keys');
+ require('./models/User');
  require('./services/passport'); //services
  require('./routes/authRoutes')(app); //authroutes
 
+ mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
 
-mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
+ app.use(
+   cookieSession({
+     maxAge: 30 * 24 * 60 * 60 * 1000,
+     keys: [keys.cookieKey]
+   })
+ );
 
+ app.use(passport.initialize());
+ app.use(passport.session());
 
  const PORT = process.env.PORT || 5000;
  app.listen(PORT) 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,31 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+//Bringing in the model 
+const User = mongoose.model('users');
+
+passport.serializeUser(( newUser, done ) => {
+  done( null, newUser.id );
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = User.findById(id); 
+  done(null, user);
+});
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(profile);
+}, 
+async (accessToken, refreshToken, profile, done) => {
+ const existingUser = await User.findOne({ googleId: profile.id })
+  if(existingUser) {
+    done(null, existingUser);
+  } else {
+    const newUser = await new User({ googleId: profile.id }).save();
+    done(null,newUser);
+  }
 }));
