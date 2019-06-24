d3c927dbeaff6bcf7122694b5172346f42749c7d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-24 13:24:52 +0530

    finishing connecting mlab dtabase to app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
  const app = require('express')(); //require express
+ const mongoose = require('mongoose');
+ const keys = require('./config/keys');
  require('./services/passport'); //services
  require('./routes/authRoutes')(app); //authroutes
 
 
+mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
 
 
  const PORT = process.env.PORT || 5000;
