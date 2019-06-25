14c3021403e839d506dd42d96174505c8e5adff3
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-25 16:10:10 +0530

    getting current user from route and logout feature

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
- const app = require('express')(); //require express
+ const express = require('express'); //require express
  const mongoose = require('mongoose');
  const cookieSession = require('cookie-session');
  const passport = require('passport');
  const keys = require('./config/keys');
  require('./models/User');
  require('./services/passport'); //services
- require('./routes/authRoutes')(app); //authroutes
-
+ const authRoutes = require('./routes/authRoutes'); //authroutes
+ 
  mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
 
+ const app = express();
+ //configuring cookie session
  app.use(
    cookieSession({
      maxAge: 30 * 24 * 60 * 60 * 1000,
      keys: [keys.cookieKey]
-   })
- );
-
- app.use(passport.initialize());
- app.use(passport.session());
-
+    })
+  );
+    
+  app.use(passport.initialize());
+  app.use(passport.session());
+  authRoutes(app); //routes
+    
  const PORT = process.env.PORT || 5000;
  app.listen(PORT) 
--- server/services/passport.js
+++ server/services/passport.js
@@ -6,26 +6,31 @@ const keys = require('../config/keys');
 //Bringing in the model 
 const User = mongoose.model('users');
 
+passport.serializeUser(( user, done ) => {
+  done( null, user.id );
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  })
+});
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, 
-async (accessToken, refreshToken, profile, done) => {
- const existingUser = await User.findOne({ googleId: profile.id })
-  if(existingUser) {
-    done(null, existingUser);
-  } else {
-    const newUser = await new User({ googleId: profile.id }).save();
-    done(null,newUser);
-  }
+ (accessToken, refreshToken, profile, done) => {
+  User.findOne({ googleId: profile.id }).then(existingUser => {
+    if(existingUser) {
+      done(null, existingUser);
+    } else {
+      new User({ googleId: profile.id })
+        .save()
+        .then(user => done(null, user));     
+    }
+  })
 }));
 
-passport.serializeUser(( newUser, done ) => {
-  done( null, newUser.id );
-});
 
-passport.deserializeUser(async (id, done) => {
-  const user = User.findById(id); 
-  done(null, user);
-});
