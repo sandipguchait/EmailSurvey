658f0f51ba6b8ea6e44e30e099121705cfb6bbf6
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-26 19:26:02 +0530

    building header UI

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const DashBoard = () => <h2>DashBoard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = (props) => {
   return (
-    <h1>Heloooooo</h1>
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={DashBoard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
