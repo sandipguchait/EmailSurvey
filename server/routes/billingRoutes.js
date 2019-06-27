8cd9c0e9079329bd0ef38cb7352582c8d0f9eaee
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-28 00:53:56 +0530

    implementing requireLogin middleware and hooking up with /api/stripe route

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,11 +1,10 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
+
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
-    if(!req.user) {
-      return res.status(401).send({ error: "You Must Log In! "})
-    }
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     //Charging the card
     const charge = await stripe.charges.create({
       amount: 500,
