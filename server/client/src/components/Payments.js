ad71414ea05b915370ee3216f548c2e235a33d48
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-27 23:12:19 +0530

    styling

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -6,12 +6,18 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout 
+        name="ESurvey"
+        description="$5 for 5 Email Credits"
         amount={500} //in cents -> $5 -> 500cents
         token={(token) =>{
           console.log(token)
         }}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
