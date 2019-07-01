7b7895ba9d3492f29de8c2f998248394b2f2ac37
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-07-01 23:27:19 +0530

    setting up routes and file path for heroku

--- index.js
+++ index.js
@@ -26,7 +26,20 @@
   app.use(passport.initialize());
   app.use(passport.session());
   authRoutes(app); //routes
-  billingRoutes(app);
+  billingRoutes(app); //routes
+
+  
+  //Seeting front end file path for heroku with routes.
+  if(process.env.NODE_ENV === 'production') {
+    //Express will serve up production assets
+    app.use(express.static('client/build'));
+
+    //Express will serve index.html if no route matches
+    const path = require('path');
+    app.get('*',(req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    });
+  }
     
  const PORT = process.env.PORT || 5000;
  app.listen(PORT) 
