4f997ad54f52369643535e02de5f45e0278ace94
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-07-18 16:46:12 +0530

    completing Mailer configuration

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,10 +1,13 @@
 //Middlewares
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-
 //Survey Model
 const mongoose = require('mongoose');
 const Survey = mongoose.model('surveys');
+// Mailer
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
+
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
@@ -20,5 +23,7 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+    //Send an Email
+    const mailer = new Mailer(survey,surveyTemplate(survey));
    });
 };
